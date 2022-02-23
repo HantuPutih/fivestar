b2ece6cd52870c1f493fb817c1f0db1bb0a03e96
Farid Juliano <thanatos.juliano@gmail.com>
2022-02-23 12:15:15 +0700

    figma, added text   and basic js

---
+++ View/script.js
@@ -0,0 +1,9 @@
+
+const submitBtn = document.getElementById('submit-btn');
+(function () {
+  console.log('masuk run')
+
+  submitBtn.addEventListener('click', () => {
+    console.log('open modal')
+  })
+})();
