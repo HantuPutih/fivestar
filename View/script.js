3c358a68887a73fe3a46712161f52234f030aca4
Farid Juliano <thanatos.juliano@gmail.com>
2022-02-24 15:25:08 +0700

    try modal

--- View/script.js
+++ View/script.js
@@ -3,7 +3,17 @@ const open_modal = document.getElementById('open_modal');
 const modal_container = document.getElementById('modal_container');
 const submit_btn = document.getElementById('submit_btn');
 
+
 (function () {
+//I'm using "click" but it works with any event
+  document.addEventListener('click', function(event) {
+    var isClickInside = modal_container.contains(event.target);
+
+    console.log(isClickInside)
+    if (!isClickInside) {
+      //the click was outside the specifiedElement, do something
+    }
+  });
 
   open_modal.addEventListener('click', (event) => {
     event.preventDefault()
@@ -13,6 +23,5 @@ const submit_btn = document.getElementById('submit_btn');
   submit_btn.addEventListener('click', (event) => {
     event.preventDefault()
     modal_container.classList.remove('show')
-
   })
 })();
