4cc170ee62411c64f10cbeff440ce0f37a12a535
Farid Juliano <thanatos.juliano@gmail.com>
2022-02-25 22:17:27 +0700

    click logic on progress

--- View/script.js
+++ View/script.js
@@ -1,18 +1,36 @@
 
 const open_modal = document.getElementById('open_modal');
 const modal_container = document.getElementById('modal_container');
+const modal_ = document.getElementById('modal_');
 const submit_btn = document.getElementById('submit_btn');
+const starsA= document.querySelectorAll(".stars a");
+
 
 
 (function () {
+  starsA.forEach((star, clickedIdx) => {
+    star.addEventListener('click', () => {
+      // otherStar.classList.add("disabled")
+      starsA.forEach((starJ, idxJ)=> {
+        console.log(idxJ, clickedIdx)
+      starJ.classList.remove("active")
+        if (idxJ <= clickedIdx) {
+          starJ.classList.add("active")
+        }
+      })
+
+      console.log(`star ${clickedIdx + 1} was clicked`) //hit to backend
+    })
+  })
+
 //I'm using "click" but it works with any event
   document.addEventListener('click', function(event) {
-    var isClickInside = modal_container.contains(event.target);
+    const isClickInside = modal_.contains(event.target);
 
-    console.log(isClickInside)
+    // console.log(modal_container.classList.contains('show'))
     if (!isClickInside) {
       // modal_container.classList.remove('show')
-      //the click was outside the specifiedElement, do something
+      //the click was outside the specifiedElement, do something  && modal_container.classList.includes('show'))
     }
   });
 
@@ -26,3 +44,5 @@ const submit_btn = document.getElementById('submit_btn');
     modal_container.classList.remove('show')
   })
 })();
+
+
